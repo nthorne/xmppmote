2c75d17c0389e03b2a24ed65984541c99cb4e0d0
nthorne <notrupertthorne@gmail.com>
2012-09-08 00:20:43 +0200

    implemented StatusProvider unit test

--- bot/test/test_statusprovider.py
+++ bot/test/test_statusprovider.py
@@ -28,8 +28,157 @@ sys.path.append(os.path.abspath("../.."))
 import mox
 import unittest
 
+from ConfigParser import SafeConfigParser
+from ConfigParser import NoOptionError
+from statusprovider import StatusProvider
+from commandhandlers import CommandHandler
+from configuration.configurationparser import ConfigurationParser
+import threading
+import subprocess
+import logging
+
 class StatusProviderTest(mox.MoxTestBase):
-    pass
+    """ This type provides test cases for the StatusProvider type. """
+
+    def __setup_parser(self):
+        mock_file = self.mox.CreateMockAnything()
+        mock_file.closed = False
+        mock_file.name = "foobar"
+
+        config = ConfigurationParser()
+        config.parse(mock_file)
+
+
+    def test_existing_status_config_section(self):
+        """ If we have an existing, properly configured status section,
+        StatusProvider will, after having been started, execute the configured
+        command after the configured timeout has elapsed, by starting a
+        threading.Timer. """
+
+        self.__setup_parser()
+
+        self.mox.StubOutWithMock(SafeConfigParser, "has_section")
+        self.mox.StubOutWithMock(SafeConfigParser, "get")
+        self.mox.StubOutWithMock(threading, "Timer")
+        self.mox.StubOutWithMock(threading.Thread, "start")
+        self.mox.StubOutWithMock(subprocess.Popen, "__init__")
+        self.mox.StubOutWithMock(subprocess.Popen, "communicate")
+        self.mox.StubOutWithMock(CommandHandler, "change_status")
+
+        SafeConfigParser.has_section("status").AndReturn(True)
+        SafeConfigParser.get("status", "command").AndReturn("foobar")
+        SafeConfigParser.get("status", "interval").AndReturn(3)
+
+        threading.Timer(3, mox.IgnoreArg())
+        threading.Thread.start()
+
+        subprocess.Popen.__init__("foobar", stdout = subprocess.PIPE)
+        subprocess.Popen.communicate().AndReturn("result")
+
+        CommandHandler.change_status("result")
+
+        threading.Timer(3, mox.IgnoreArg())
+        threading.Thread.start()
+
+        self.mox.ReplayAll()
+
+        provider = StatusProvider()
+        provider.start()
+        # We'll have to emulate a timeout here..
+        provider.timeout()
+
+    def test_nonexisting_status_config_section(self):
+        """ If there is no status section in the configuration file, no timer
+        will be started, and no status updates will be performed. """
+
+        self.__setup_parser()
+
+        self.mox.StubOutWithMock(SafeConfigParser, "has_section")
+
+        SafeConfigParser.has_section("status").AndReturn(False)
+
+        self.mox.ReplayAll()
+
+        provider = StatusProvider()
+        provider.start()
+
+    def test_existing_status_config_section_missing_interval(self):
+        """ If the command execution frequency is missing from the
+        configutation, no action will be taken (as with the case of missing
+        status section). """
+
+        self.__setup_parser()
+
+        self.mox.StubOutWithMock(SafeConfigParser, "has_section")
+        self.mox.StubOutWithMock(SafeConfigParser, "get")
+
+        SafeConfigParser.has_section("status").AndReturn(True)
+        SafeConfigParser.get("status", "command").AndReturn("uptime")
+        SafeConfigParser.get("status",
+                             "interval").AndRaise(NoOptionError("status",
+                                                                "interval"))
+
+        self.mox.ReplayAll()
+
+        provider = StatusProvider()
+        provider.start()
+
+    def test_existing_status_config_section_missing_command(self):
+        """ If the command itself is missing from the section, no action will be
+        taken (as with the case of missing status section). """
+
+        self.__setup_parser()
+
+        self.mox.StubOutWithMock(SafeConfigParser, "has_section")
+        self.mox.StubOutWithMock(SafeConfigParser, "get")
+
+        SafeConfigParser.has_section("status").AndReturn(True)
+        SafeConfigParser.get("status",
+                             "command").AndRaise(NoOptionError("status",
+                                                               "command"))
+
+        self.mox.ReplayAll()
+
+        provider = StatusProvider()
+        provider.start()
+
+    def test_existing_status_config_section_erroneous_command(self):
+        """ If the command is configured, but does not exist, status will not be
+        updated, but a log statement will be written to the syslog to indicate
+        the configuration error. """
+
+        self.__setup_parser()
+
+        mock_logger = self.mox.CreateMockAnything()
+
+        self.mox.StubOutWithMock(SafeConfigParser, "has_section")
+        self.mox.StubOutWithMock(SafeConfigParser, "get")
+        self.mox.StubOutWithMock(threading, "Timer")
+        self.mox.StubOutWithMock(threading.Thread, "start")
+        self.mox.StubOutWithMock(subprocess.Popen, "__init__")
+        self.mox.StubOutWithMock(subprocess.Popen, "communicate")
+        self.mox.StubOutWithMock(logging, "getLogger")
+        self.mox.StubOutWithMock(CommandHandler, "change_status")
+
+        SafeConfigParser.has_section("status").AndReturn(True)
+        SafeConfigParser.get("status", "command").AndReturn("foobar")
+        SafeConfigParser.get("status", "interval").AndReturn(3)
+
+        threading.Timer(3, mox.IgnoreArg())
+        threading.Thread.start()
+
+        subprocess.Popen.__init__("foobar", stdout = subprocess.PIPE)
+        subprocess.Popen.communicate().AndRaise(OSError)
+
+        logging.getLogger().AndReturn(mock_logger)
+        mock_logger.error("%s: error executing status command.")
+
+        self.mox.ReplayAll()
+
+        provider = StatusProvider()
+        provider.start()
+        # We'll have to emulate a timeout here..
+        provider.timeout()
 
 
 if "__main__" == __name__:
