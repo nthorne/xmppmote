3dcb42a4135ad511b295952495cc8466813f0109
nthorne <notrupertthorne@gmail.com>
2012-09-17 23:33:15 +0200

    implemented StableUpdater.check and its unit test

--- lib/guf/stableupdater.py
+++ lib/guf/stableupdater.py
@@ -22,12 +22,87 @@
 StableUpdater is responsible for performing stable version checks and updates of
 the software, against github. """
 
+import sys
+import os
+
+sys.path.append(os.path.abspath("../.."))
+
 from updater import Updater
 
+import urllib2
+import json
+import base64
+
+import version
+import re
+
+import logging
 
 class StableUpdater(Updater):
     """ StableUpdater queries github for lates tag, and if that differs from the
     version defined in version.version, a tarball for the newer tag is
     downloaded. """ 
 
-    pass
+    def __init__(self, remote_url):
+        self.__remote_url = remote_url
+
+        self.has_new_version = False
+        self.remote_version = None
+
+        self.__version_regex = re.compile(r"version\s*=.*")
+        self.__version_get_regex = re.compile(r"^\s*version[^0-9]*([.0-9]+).*$")
+        self.__invalid_version_token = re.compile(r"[^.0-9]")
+
+
+    def check(self):
+        """ Check __remote_url for an new software version. This is done via the
+        github json API, where we download the version file, and inspect its
+        contents. """
+
+        self.has_new_version = False
+        self.remote_version = None
+
+        logger = logging.getLogger()
+
+        try:
+            response = urllib2.urlopen(self.__remote_url)
+        except (ValueError, urllib2.URLError), exc:
+            logger.info(u"Failed to connect to %s: %s" % (self.__remote_url,
+                                                          exc))
+            return
+
+        html = response.read()
+
+        if not html:
+            return
+
+        try:
+            json_dict = json.loads(html)
+        except ValueError:
+            return
+
+        try:
+            remote_src = base64.b64decode(json_dict["content"])
+        except TypeError:
+            return
+
+        self.remote_version = self.__parse_remote_version(remote_src)
+
+        if self.remote_version and hasattr(version, "version"):
+            self.has_new_version = version.version < self.remote_version
+
+    def __parse_remote_version(self, remote_src):
+        """ Parse the string contents of the version file, attempting to extract
+        the version defined in it. """
+
+        ver = re.findall(self.__version_regex, remote_src)
+
+        result = None
+
+        if ver and 1 == len(ver):
+            result = re.sub(self.__version_get_regex, r"\1", ver[0])
+            if re.findall(self.__invalid_version_token, result):
+                result = None
+
+        return result
+
--- lib/guf/test/test_stableupdater.py
+++ lib/guf/test/test_stableupdater.py
@@ -23,17 +23,414 @@ import sys
 import os
 
 sys.path.append(os.path.abspath(".."))
+sys.path.append(os.path.abspath("../../.."))
 
 import mox
 import unittest
 
-import stableupdater
+import urllib2
+import json
+import base64
+
+import version
+
+from stableupdater import StableUpdater
 
 
 class StableUpdaterTest(mox.MoxTestBase):
     """ This type provides test cases for StableUpdater. """
 
-    pass
+    __remote_url = "there is no url"
+    __mocked_remote_version = "1.3.3.7"
+    __mock_file_with_proper_version = """
+# This is a properly formed version file.
+
+version = %s
+""" % __mocked_remote_version
+    __mock_file_without_version = """
+# This one just contains
+# a couple of lines of
+# comments.
+"""
+    __mock_file_with_multiple_versions = """
+# .. and this one contains a whole bunch of version definitions
+version = "1.3.3.7"
+version = "1.3.3.8"
+"""
+    __mock_file_with_malformed_version = """
+version =
+"""
+
+    def test_github_has_new_version(self):
+        """ Ensure proper behavior when we receive a remote version file that
+        details a version newer than the current one. """
+
+        version.version = "0.0"
+
+        mock_response = self.mox.CreateMockAnything()
+        mock_html = self.mox.CreateMockAnything()
+        mock_json_dict = self.mox.CreateMockAnything()
+
+        self.mox.StubOutWithMock(urllib2, "urlopen")
+        self.mox.StubOutWithMock(json, "loads")
+        self.mox.StubOutWithMock(base64, "b64decode")
+
+        urllib2.urlopen(self.__remote_url).AndReturn(mock_response)
+        mock_response.read().AndReturn(mock_html)
+
+        json.loads(mock_html).AndReturn(mock_json_dict)
+
+        base64.b64decode(mock_json_dict["content"].AndReturn(
+            self.__mock_file_with_proper_version)).AndReturn(
+                self.__mock_file_with_proper_version)
+
+        self.mox.ReplayAll()
+
+        stable_updater = StableUpdater(self.__remote_url)
+        stable_updater.check()
+
+        self.assertTrue(stable_updater.has_new_version)
+        self.assertEquals(self.__mocked_remote_version,
+                          stable_updater.remote_version)
+
+    def test_github_does_not_have_new_version(self):
+        """ Make sure that no update is indicated if the remote version euqals
+        the local one. """
+
+        version.version = self.__mocked_remote_version
+
+        mock_response = self.mox.CreateMockAnything()
+        mock_html = self.mox.CreateMockAnything()
+        mock_json_dict = self.mox.CreateMockAnything()
+
+        self.mox.StubOutWithMock(urllib2, "urlopen")
+        self.mox.StubOutWithMock(json, "loads")
+        self.mox.StubOutWithMock(base64, "b64decode")
+
+        urllib2.urlopen(self.__remote_url).AndReturn(mock_response)
+        mock_response.read().AndReturn(mock_html)
+
+        json.loads(mock_html).AndReturn(mock_json_dict)
+
+        base64.b64decode(mock_json_dict["content"].AndReturn(
+            self.__mock_file_with_proper_version)).AndReturn(
+                self.__mock_file_with_proper_version)
+
+        self.mox.ReplayAll()
+
+        stable_updater = StableUpdater(self.__remote_url)
+        stable_updater.check()
+
+        self.assertTrue(not stable_updater.has_new_version)
+        self.assertEquals(version.version,
+                          stable_updater.remote_version)
+
+    def test_github_not_responding(self):
+        """ Make sure that the updater handles unresponsive remote servers
+        properly. """
+
+        version.version = "0.0"
+
+        self.mox.StubOutWithMock(urllib2, "urlopen")
+
+        urllib2.urlopen(self.__remote_url).AndRaise(urllib2.URLError("Timeout"))
+
+        self.mox.ReplayAll()
+
+        stable_updater = StableUpdater(self.__remote_url)
+        stable_updater.check()
+
+        self.assertTrue(not stable_updater.has_new_version)
+        self.assertEquals(None, stable_updater.remote_version)
+
+    def test_github_response_no_content(self):
+        """ Make sure that a request response without any JSON content is
+        handled gracefully. """
+
+        version.version = "0.0"
+
+        mock_response = self.mox.CreateMockAnything()
+        mock_html = self.mox.CreateMockAnything()
+
+        self.mox.StubOutWithMock(urllib2, "urlopen")
+        self.mox.StubOutWithMock(json, "loads")
+
+        urllib2.urlopen(self.__remote_url).AndReturn(mock_response)
+        mock_response.read().AndReturn(mock_html)
+
+        json.loads(mock_html).AndRaise(ValueError("No JSON object"))
+
+        self.mox.ReplayAll()
+
+        stable_updater = StableUpdater(self.__remote_url)
+        stable_updater.check()
+
+        self.assertTrue(not stable_updater.has_new_version)
+        self.assertEquals(None, stable_updater.remote_version)
+
+    def test_no_version_version_defined(self):
+        """ Make sure that the updater handles an erroneous (missing) local
+        version gracefully. """
+
+        del version.version
+
+        mock_response = self.mox.CreateMockAnything()
+        mock_html = self.mox.CreateMockAnything()
+        mock_json_dict = self.mox.CreateMockAnything()
+
+        self.mox.StubOutWithMock(urllib2, "urlopen")
+        self.mox.StubOutWithMock(json, "loads")
+        self.mox.StubOutWithMock(base64, "b64decode")
+
+        urllib2.urlopen(self.__remote_url).AndReturn(mock_response)
+        mock_response.read().AndReturn(mock_html)
+
+        json.loads(mock_html).AndReturn(mock_json_dict)
+
+        base64.b64decode(mock_json_dict["content"].AndReturn(
+            self.__mock_file_with_proper_version)).AndReturn(
+                self.__mock_file_with_proper_version)
+
+        self.mox.ReplayAll()
+
+        stable_updater = StableUpdater(self.__remote_url)
+        stable_updater.check()
+
+        self.assertTrue(not stable_updater.has_new_version)
+
+    def test_no_remote_version(self):
+        """ If there is no remote version to be found, ensure that no updates
+        are indicated. """
+
+        version.version = "0.0"
+
+        mock_response = self.mox.CreateMockAnything()
+        mock_html = self.mox.CreateMockAnything()
+        mock_json_dict = self.mox.CreateMockAnything()
+
+        self.mox.StubOutWithMock(urllib2, "urlopen")
+        self.mox.StubOutWithMock(json, "loads")
+        self.mox.StubOutWithMock(base64, "b64decode")
+
+        urllib2.urlopen(self.__remote_url).AndReturn(mock_response)
+        mock_response.read().AndReturn(mock_html)
+
+        json.loads(mock_html).AndReturn(mock_json_dict)
+
+        base64.b64decode(mock_json_dict["content"].AndReturn(
+            self.__mock_file_without_version)).AndReturn(
+                self.__mock_file_without_version)
+
+        self.mox.ReplayAll()
+
+        stable_updater = StableUpdater(self.__remote_url)
+        stable_updater.check()
+
+        self.assertTrue(not stable_updater.has_new_version)
+        self.assertEquals(None, stable_updater.remote_version)
+
+    def test_mutiple_remote_versions(self):
+        """ In case the remote version file contains multiple version
+        definitions, make sure that no update is indicated. """
+
+        version.version = "0.0"
+
+        mock_response = self.mox.CreateMockAnything()
+        mock_html = self.mox.CreateMockAnything()
+        mock_json_dict = self.mox.CreateMockAnything()
+
+        self.mox.StubOutWithMock(urllib2, "urlopen")
+        self.mox.StubOutWithMock(json, "loads")
+        self.mox.StubOutWithMock(base64, "b64decode")
+
+        urllib2.urlopen(self.__remote_url).AndReturn(mock_response)
+        mock_response.read().AndReturn(mock_html)
+
+        json.loads(mock_html).AndReturn(mock_json_dict)
+
+        base64.b64decode(mock_json_dict["content"].AndReturn(
+            self.__mock_file_with_multiple_versions)).AndReturn(
+                self.__mock_file_with_multiple_versions)
+
+        self.mox.ReplayAll()
+
+        stable_updater = StableUpdater(self.__remote_url)
+        stable_updater.check()
+
+        self.assertTrue(not stable_updater.has_new_version)
+        self.assertEquals(None, stable_updater.remote_version)
+
+    def test_empty_response_read(self):
+        """ Empty HTML/JSON should be handled gracefully. """
+
+        version.version = "0.0"
+
+        mock_response = self.mox.CreateMockAnything()
+
+        self.mox.StubOutWithMock(urllib2, "urlopen")
+        self.mox.StubOutWithMock(json, "loads")
+        self.mox.StubOutWithMock(base64, "b64decode")
+
+        urllib2.urlopen(self.__remote_url).AndReturn(mock_response)
+        mock_response.read().AndReturn("")
+
+        self.mox.ReplayAll()
+
+        stable_updater = StableUpdater(self.__remote_url)
+        stable_updater.check()
+
+        self.assertTrue(not stable_updater.has_new_version)
+        self.assertEquals(None, stable_updater.remote_version)
+
+    def test_malformed_base64_string(self):
+        """ Make sure malformed base64 is handled gracefully. """
+
+        version.version = "0.0"
+
+        mock_response = self.mox.CreateMockAnything()
+        mock_html = self.mox.CreateMockAnything()
+        mock_json_dict = self.mox.CreateMockAnything()
+
+        self.mox.StubOutWithMock(urllib2, "urlopen")
+        self.mox.StubOutWithMock(json, "loads")
+        self.mox.StubOutWithMock(base64, "b64decode")
+
+        urllib2.urlopen(self.__remote_url).AndReturn(mock_response)
+        mock_response.read().AndReturn(mock_html)
+
+        json.loads(mock_html).AndReturn(mock_json_dict)
+
+        base64.b64decode(mock_json_dict["content"].AndReturn(
+            self.__mock_file_with_proper_version)).AndRaise(
+                TypeError("Invalid Base64"))
+
+        self.mox.ReplayAll()
+
+        stable_updater = StableUpdater(self.__remote_url)
+        stable_updater.check()
+
+        self.assertTrue(not stable_updater.has_new_version)
+        self.assertEquals(None, stable_updater.remote_version)
+
+    def test_invalid_url(self):
+        """ Invalid URL should be handled gracefully. """
+
+        version.version = "0.0"
+
+        self.mox.StubOutWithMock(urllib2, "urlopen")
+
+        urllib2.urlopen(self.__remote_url).AndRaise(
+            ValueError("Unknown url type"))
+
+        self.mox.ReplayAll()
+
+        stable_updater = StableUpdater(self.__remote_url)
+        stable_updater.check()
+
+        self.assertTrue(not stable_updater.has_new_version)
+        self.assertEquals(None, stable_updater.remote_version)
+
+    def test_malformed_version_variable_definition(self):
+        """ Malformed remote version variables should be handled gracefully.
+        """
+
+        version.version = self.__mocked_remote_version
+
+        mock_response = self.mox.CreateMockAnything()
+        mock_html = self.mox.CreateMockAnything()
+        mock_json_dict = self.mox.CreateMockAnything()
+
+        self.mox.StubOutWithMock(urllib2, "urlopen")
+        self.mox.StubOutWithMock(json, "loads")
+        self.mox.StubOutWithMock(base64, "b64decode")
+
+        urllib2.urlopen(self.__remote_url).AndReturn(mock_response)
+        mock_response.read().AndReturn(mock_html)
+
+        json.loads(mock_html).AndReturn(mock_json_dict)
+
+        base64.b64decode(mock_json_dict["content"].AndReturn(
+            self.__mock_file_with_malformed_version)).AndReturn(
+                self.__mock_file_with_malformed_version)
+
+        self.mox.ReplayAll()
+
+        stable_updater = StableUpdater(self.__remote_url)
+        stable_updater.check()
+
+        self.assertTrue(not stable_updater.has_new_version)
+        self.assertEquals(None, stable_updater.remote_version)
+
+    def test_version_check_fails_second_time(self):
+        """ If a second version check fails, make sure that the update
+        availability statuses reflects that. """
+
+        version.version = "0.0"
+
+        mock_response = self.mox.CreateMockAnything()
+        mock_html = self.mox.CreateMockAnything()
+        mock_json_dict = self.mox.CreateMockAnything()
+
+        self.mox.StubOutWithMock(urllib2, "urlopen")
+        self.mox.StubOutWithMock(json, "loads")
+        self.mox.StubOutWithMock(base64, "b64decode")
+
+        urllib2.urlopen(self.__remote_url).AndReturn(mock_response)
+        mock_response.read().AndReturn(mock_html)
+
+        json.loads(mock_html).AndReturn(mock_json_dict)
+
+        base64.b64decode(mock_json_dict["content"].AndReturn(
+            self.__mock_file_with_proper_version)).AndReturn(
+                self.__mock_file_with_proper_version)
+
+        urllib2.urlopen(self.__remote_url).AndRaise(urllib2.URLError("Timeout"))
+
+        self.mox.ReplayAll()
+
+        stable_updater = StableUpdater(self.__remote_url)
+        stable_updater.check()
+
+        self.assertTrue(stable_updater.has_new_version)
+        self.assertEquals(self.__mocked_remote_version,
+                          stable_updater.remote_version)
+
+        stable_updater.check()
+
+        self.assertTrue(not stable_updater.has_new_version)
+        self.assertEquals(None, stable_updater.remote_version)
+
+    def test_github_has_old_version(self):
+        """ Ensure proper behavior when we receive a remote version file that
+        details a version older than the current one. """
+
+        version.version = "1.3.3.8"
+
+        mock_response = self.mox.CreateMockAnything()
+        mock_html = self.mox.CreateMockAnything()
+        mock_json_dict = self.mox.CreateMockAnything()
+
+        self.mox.StubOutWithMock(urllib2, "urlopen")
+        self.mox.StubOutWithMock(json, "loads")
+        self.mox.StubOutWithMock(base64, "b64decode")
+
+        urllib2.urlopen(self.__remote_url).AndReturn(mock_response)
+        mock_response.read().AndReturn(mock_html)
+
+        json.loads(mock_html).AndReturn(mock_json_dict)
+
+        base64.b64decode(mock_json_dict["content"].AndReturn(
+            self.__mock_file_with_proper_version)).AndReturn(
+                self.__mock_file_with_proper_version)
+
+        self.mox.ReplayAll()
+
+        stable_updater = StableUpdater(self.__remote_url)
+        stable_updater.check()
+
+        self.assertTrue(not stable_updater.has_new_version)
+        self.assertEquals(self.__mocked_remote_version,
+                          stable_updater.remote_version)
 
 
 if "__main__" == __name__:
