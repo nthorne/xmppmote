d329e752c3f74ef033384921da63ab911c7a27fa
nthorne <notrupertthorne@gmail.com>
2012-09-21 23:24:18 +0200

    implemented BleedingEdgeUpdater and its unit test

--- lib/guf/bleedingedgeupdater.py
+++ lib/guf/bleedingedgeupdater.py
@@ -22,8 +22,18 @@
 BleedingEdgeUpdater is responsible for performing bleeding edge version checks
 and updates of the software, against github. """
 
+import sys
+import os
+
+sys.path.append(os.path.abspath("../../.."))
+
+
 from updater import Updater
 
+import git
+import version
+import urllib2
+import json
 
 class BleedingEdgeUpdater(Updater):
     """ BleedingEdgeUpdater queries github for current commit id, and if that
@@ -32,5 +42,75 @@ class BleedingEdgeUpdater(Updater):
     possible; otherwise a tarball of HEAD of origin/master is downloaded. """
 
     def __init__(self, remote_url):
-        pass
+        super(BleedingEdgeUpdater, self).__init__()
+
+        self.__remote_url = remote_url
+        self.__project_root = os.path.dirname(
+            os.path.abspath(version.__file__))
+
+        self.local_head_commit_hash = None
+        self.origin_head_sha = None
+
+        if self.is_repo():
+            self.repo = git.LocalRepository(self.__project_root)
+
+            if self.has_git():
+                self.local_head_commit_hash = self.repo.getHead().hash
+
+    def is_repo(self):
+        """ Test if the project root is a git repo (i.e. has a .git subfolder).
+        """
+
+        return os.path.isdir(os.path.join(self.__project_root, ".git"))
+
+    def has_git(self):
+        """ Test if git v1 is available. """
+
+        try:
+            result = "1." == self.repo.getGitVersion()[:2]
+        except (git.exceptions.GitException, AttributeError):
+            result = False
+
+        return result
+
+    def get_origin_head_sha(self):
+        """ Return the SHA of origin/HEAD. """
+
+        result = None
+
+        try:
+            response = urllib2.urlopen(self.__remote_url)
+
+            html = response.read()
+
+            json_dict = json.loads(html)
+
+            result = json_dict["sha"]
+        # raised from urlopen
+        except urllib2.URLError:
+            pass
+        # raised if urlopen returns None
+        except AttributeError:
+            pass
+        # raised upon None or invalid json data
+        except (TypeError, ValueError):
+            pass
+        # raised if the sha key does not exists in the json dict
+        except KeyError:
+            pass
+
+        return result
+
+    def check(self):
+        """ Check __remote_url for a new software version. This is done via the
+        github json API, where we query origin for the HEAD SHA, and compare it
+        to the local HEAD SHA. """
+
+        new_version = False
+
+        self.origin_head_sha = self.get_origin_head_sha()
+        if self.origin_head_sha:
+            new_version = self.local_head_commit_hash != self.origin_head_sha
+
+        return new_version
 
--- lib/guf/test/test_bleedingedgeupdater.py
+++ lib/guf/test/test_bleedingedgeupdater.py
@@ -27,12 +27,324 @@ sys.path.append(os.path.abspath(".."))
 import mox
 import unittest
 
-import bleedingedgeupdater
+from bleedingedgeupdater import BleedingEdgeUpdater
+import version
+import git
+import urllib2
+import json
+
 
 class BleedingEdgeUpdaterTest(mox.MoxTestBase):
     """ Provides test cases for BleedingEdgeUpdater. """
 
-    pass
+    __project_root = os.path.dirname(
+        os.path.abspath(version.__file__))
+    __remote_url = "there is no remote"
+    __mock_local_head_hash = "local"
+    __mock_origin_head_sha = "origin"
+
+
+    def test_is_repo_when_is_repo(self):
+        """ If project root contains a .git directory, make sure that is_repo
+        indicates that. """
+
+        self.mox.StubOutWithMock(os.path, "isdir")
+
+        # This first call is __init__ checking is_repo in order to create a
+        # LocalRepository attribute.
+        os.path.isdir(os.path.join(self.__project_root, ".git")).AndReturn(True)
+        os.path.isdir(os.path.join(self.__project_root, ".git")).AndReturn(True)
+
+        self.mox.ReplayAll()
+
+        updater = BleedingEdgeUpdater(self.__remote_url)
+        self.assertTrue(updater.is_repo())
+
+    def test_is_repo_when_isnt_repo(self):
+        """ If project root does not contain a .git directory, make sure that
+        is_repo indicates that. """
+
+        self.mox.StubOutWithMock(os.path, "isdir")
+
+        # This first call is __init__ checking is_repo in order to create a
+        # LocalRepository attribute.
+        os.path.isdir(os.path.join(
+            self.__project_root, ".git")).AndReturn(False)
+        os.path.isdir(os.path.join(
+            self.__project_root, ".git")).AndReturn(False)
+
+        self.mox.ReplayAll()
+
+        updater = BleedingEdgeUpdater(self.__remote_url)
+        self.assertTrue(not updater.is_repo())
+
+    def test_has_git_when_has_git(self):
+        """ has_git should return True if the git library indicates git v1 as
+        being available. """
+
+        self.mox.StubOutWithMock(git.LocalRepository, "getGitVersion")
+
+        # This first call is __init__ retrieving HEAD commit hash
+        git.LocalRepository.getGitVersion().AndReturn("1.7.5.4")
+        git.LocalRepository.getGitVersion().AndReturn("1.7.5.4")
+
+        self.mox.ReplayAll()
+
+        updater = BleedingEdgeUpdater(self.__remote_url)
+        self.assertTrue(updater.has_git())
+
+    def test_has_git_when_does_not_have_git(self):
+        """ has_git should return False if the git library is unable to locate
+        git v1. """
+
+        self.mox.StubOutWithMock(git.LocalRepository, "getGitVersion")
+
+        # This first call is __init__ retrieving HEAD commit hash
+        git.LocalRepository.getGitVersion().AndReturn("1.7.5.4")
+        git.LocalRepository.getGitVersion().AndRaise(
+            git.exceptions.GitException("dang nabit"))
+
+        self.mox.ReplayAll()
+
+        updater = BleedingEdgeUpdater(self.__remote_url)
+        self.assertTrue(not updater.has_git())
+
+    def test_has_git_when_is_not_repo(self):
+        """ If project root is not a repo, has_git should return False, rather
+        than raising an exception, as it requires a local repository in order to
+        query git for version(?!). """
+
+        self.mox.StubOutWithMock(BleedingEdgeUpdater, "is_repo")
+        self.mox.StubOutWithMock(git.LocalRepository, "getGitVersion")
+
+        BleedingEdgeUpdater.is_repo().AndReturn(False)
+
+        self.mox.ReplayAll()
+
+        updater = BleedingEdgeUpdater(self.__remote_url)
+        self.assertTrue(not updater.has_git())
+
+    def test_getting_local_commit_hash(self):
+        """ If project root is a git repo, and git is avaialable, tha
+        local_head_commit_hash attribute should contain the SHA of HEAD after
+        having initialized BleedingEdgeUpdater. """
+
+        mock_commit = self.mox.CreateMockAnything()
+        mock_commit.hash = self.__mock_local_head_hash
+
+        self.mox.StubOutWithMock(git.LocalRepository, "getHead")
+        self.mox.StubOutWithMock(BleedingEdgeUpdater, "is_repo")
+        self.mox.StubOutWithMock(BleedingEdgeUpdater, "has_git")
+
+        BleedingEdgeUpdater.is_repo().AndReturn(True)
+        BleedingEdgeUpdater.has_git().AndReturn(True)
+        git.LocalRepository.getHead().AndReturn(mock_commit)
+
+        self.mox.ReplayAll()
+
+        updater = BleedingEdgeUpdater(self.__remote_url)
+        self.assertEquals(self.__mock_local_head_hash,
+                          updater.local_head_commit_hash)
+
+    def test_getting_local_commit_hash_no_repo(self):
+        """ If project root is not a repo, local_head_commit_hash attribute
+        should equal None. """
+        mock_commit = self.mox.CreateMockAnything()
+        mock_commit.hash = self.__mock_local_head_hash
+
+        self.mox.StubOutWithMock(git.LocalRepository, "getHead")
+        self.mox.StubOutWithMock(BleedingEdgeUpdater, "is_repo")
+        self.mox.StubOutWithMock(BleedingEdgeUpdater, "has_git")
+
+        BleedingEdgeUpdater.is_repo().AndReturn(False)
+
+        self.mox.ReplayAll()
+
+        updater = BleedingEdgeUpdater(self.__remote_url)
+        self.assertEqual(None, updater.local_head_commit_hash)
+
+    def test_getting_local_commit_hash_no_git(self):
+        """ If git is not available, local_head_commit_hash attribute should
+        equal None. """
+        
+        mock_commit = self.mox.CreateMockAnything()
+        mock_commit.hash = self.__mock_local_head_hash
+
+        self.mox.StubOutWithMock(git.LocalRepository, "getHead")
+        self.mox.StubOutWithMock(BleedingEdgeUpdater, "is_repo")
+        self.mox.StubOutWithMock(BleedingEdgeUpdater, "has_git")
+
+        BleedingEdgeUpdater.is_repo().AndReturn(True)
+        BleedingEdgeUpdater.has_git().AndReturn(False)
+
+        self.mox.ReplayAll()
+
+        updater = BleedingEdgeUpdater(self.__remote_url)
+        self.assertEqual(None, updater.local_head_commit_hash)
+
+    def test_getting_origin_head_hash(self):
+        """ Sunshine case when querying github for origin/master HEAD SHA. """
+
+        mock_response = self.mox.CreateMockAnything()
+        mock_html = self.mox.CreateMockAnything()
+        mock_json_dict = self.mox.CreateMockAnything()
+
+        self.mox.StubOutWithMock(urllib2, "urlopen")
+        self.mox.StubOutWithMock(json, "loads")
+
+        urllib2.urlopen(self.__remote_url).AndReturn(mock_response)
+        mock_response.read().AndReturn(mock_html)
+
+        json.loads(mock_html).AndReturn(mock_json_dict)
+
+        mock_json_dict["sha"].AndReturn(self.__mock_origin_head_sha)
+
+        self.mox.ReplayAll()
+
+        bleeding_updater = BleedingEdgeUpdater(self.__remote_url)
+
+        self.assertEquals(self.__mock_origin_head_sha,
+                          bleeding_updater.get_origin_head_sha())
+
+    def test_getting_origin_head_hash_urlopen_exception(self):
+        """ If urlopen raises an exception (timeout, invalid url, etc.), None
+        should be returned. """
+
+        self.mox.StubOutWithMock(urllib2, "urlopen")
+        self.mox.StubOutWithMock(json, "loads")
+
+        urllib2.urlopen(self.__remote_url).AndRaise(urllib2.URLError("na-ah"))
+
+        self.mox.ReplayAll()
+
+        bleeding_updater = BleedingEdgeUpdater(self.__remote_url)
+
+        self.assertEquals(None, bleeding_updater.get_origin_head_sha())
+
+    def test_getting_origin_head_hash_no_response(self):
+        """ If urlopen returns None, None should be returned. """
+
+        self.mox.StubOutWithMock(urllib2, "urlopen")
+        self.mox.StubOutWithMock(json, "loads")
+
+        urllib2.urlopen(self.__remote_url).AndReturn(None)
+
+        self.mox.ReplayAll()
+
+        bleeding_updater = BleedingEdgeUpdater(self.__remote_url)
+
+        self.assertEquals(None, bleeding_updater.get_origin_head_sha())
+
+    def test_getting_origin_head_hash_no_html(self):
+        """ If we fail to read the urlopen query, None shall be returned. """
+
+        mock_response = self.mox.CreateMockAnything()
+
+        self.mox.StubOutWithMock(urllib2, "urlopen")
+
+        urllib2.urlopen(self.__remote_url).AndReturn(mock_response)
+
+        mock_response.read().AndReturn(None)
+
+        self.mox.ReplayAll()
+
+        bleeding_updater = BleedingEdgeUpdater(self.__remote_url)
+
+        self.assertEquals(None, bleeding_updater.get_origin_head_sha())
+
+    def test_getting_origin_head_hash_no_json(self):
+        """ In the case of the read HTML not containing proper json, None shall
+        be returned. """
+
+        mock_response = self.mox.CreateMockAnything()
+        mock_html = "no json for you"
+
+        self.mox.StubOutWithMock(urllib2, "urlopen")
+
+        urllib2.urlopen(self.__remote_url).AndReturn(mock_response)
+
+        mock_response.read().AndReturn(mock_html)
+
+        self.mox.ReplayAll()
+
+        bleeding_updater = BleedingEdgeUpdater(self.__remote_url)
+
+        self.assertEquals(None, bleeding_updater.get_origin_head_sha())
+
+    def test_getting_origin_head_hash_no_sha_in_json(self):
+        """ If the json response from github does not contain a SHA key, None
+        shall be returned. """
+
+        mock_response = self.mox.CreateMockAnything()
+        mock_html = self.mox.CreateMockAnything()
+
+        mock_json_dict = {'foo': 'bar'}
+
+        self.mox.StubOutWithMock(urllib2, "urlopen")
+        self.mox.StubOutWithMock(json, "loads")
+
+        urllib2.urlopen(self.__remote_url).AndReturn(mock_response)
+        mock_response.read().AndReturn(mock_html)
+
+        json.loads(mock_html).AndReturn(mock_json_dict)
+
+        self.mox.ReplayAll()
+
+        bleeding_updater = BleedingEdgeUpdater(self.__remote_url)
+
+        self.assertEquals(None, bleeding_updater.get_origin_head_sha())
+
+    def test_github_has_new_version(self):
+        """ If the github query returns a SHA key that differs from the local
+        master HEAD key, check shall indicate that a new version is available.
+        """
+
+        mock_commit = self.mox.CreateMockAnything()
+        mock_commit.hash = self.__mock_local_head_hash
+
+        self.mox.StubOutWithMock(git.LocalRepository, "getHead")
+        self.mox.StubOutWithMock(BleedingEdgeUpdater, "is_repo")
+        self.mox.StubOutWithMock(BleedingEdgeUpdater, "has_git")
+        self.mox.StubOutWithMock(BleedingEdgeUpdater, "get_origin_head_sha")
+
+        BleedingEdgeUpdater.is_repo().AndReturn(True)
+        BleedingEdgeUpdater.has_git().AndReturn(True)
+        git.LocalRepository.getHead().AndReturn(mock_commit)
+
+        BleedingEdgeUpdater.get_origin_head_sha().AndReturn(
+            self.__mock_origin_head_sha)
+
+        self.mox.ReplayAll()
+
+        updater = BleedingEdgeUpdater(self.__remote_url)
+
+        self.assertTrue(updater.check())
+
+    def test_github_does_not_have_new_version(self):
+        """ If the github query returns a SHA key that is equal to the local
+        master HEAD key, check shall indicate that there is no new version
+        available. """
+
+        mock_commit = self.mox.CreateMockAnything()
+        mock_commit.hash = self.__mock_local_head_hash
+
+        self.mox.StubOutWithMock(git.LocalRepository, "getHead")
+        self.mox.StubOutWithMock(BleedingEdgeUpdater, "is_repo")
+        self.mox.StubOutWithMock(BleedingEdgeUpdater, "has_git")
+        self.mox.StubOutWithMock(BleedingEdgeUpdater, "get_origin_head_sha")
+
+        BleedingEdgeUpdater.is_repo().AndReturn(True)
+        BleedingEdgeUpdater.has_git().AndReturn(True)
+        git.LocalRepository.getHead().AndReturn(mock_commit)
+
+        BleedingEdgeUpdater.get_origin_head_sha().AndReturn(
+            self.__mock_local_head_hash)
+
+        self.mox.ReplayAll()
+
+        updater = BleedingEdgeUpdater(self.__remote_url)
+
+        self.assertTrue(not updater.check())
 
 
 if "__main__" == __name__:
